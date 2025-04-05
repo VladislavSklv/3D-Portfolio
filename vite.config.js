9c82b62d1fe94c43946a604e0caa506d735a847f
VladislavSklv <79214579055@yandex.ru>
2025-04-05 19:09:02 +0300

    chaged vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/3D-Portfolio/',
+  base: '/3D-Portfolio/', // Must match your repo name
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    assetsDir: 'assets', // Ensures assets go to /assets
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   }
 })
