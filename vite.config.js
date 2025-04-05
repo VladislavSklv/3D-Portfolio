0a508f0d919b22ae583c0745ca0d5043323339a7
VladislavSklv <79214579055@yandex.ru>
2025-04-05 18:50:35 +0300

    chaged vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/3D-Portfolio/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
